c80d9ede00b8e8d8a867d88d8fe9b727f9aaf1fa
Amgart <amg.art@icloud.com>
2021-03-22 16:35:54 +0100

    Initial Commit

---
+++ Shared/iRacingPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRacingPlannerApp.swift
+//  Shared
+//
+//  Created by Albert Martínez Genís on 22/3/21.
+//
+
+import SwiftUI
+
+@main
+struct iRacingPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
