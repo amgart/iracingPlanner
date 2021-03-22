d21c3ff46086c40efc3d7aa2608b880a72fd7a0d
Amgart <amg.art@icloud.com>
2021-03-22 16:36:59 +0100

    Hello world commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,11 +7,12 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
-    }
+    } 
 }
 
 struct ContentView_Previews: PreviewProvider {
