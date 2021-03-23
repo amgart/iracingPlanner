218c8eaba76ff6e0c58fbaf3c89457f1f7f09d52
Amgart <amg.art@icloud.com>
2021-03-23 12:34:58 +0100

    App Menu includes: * Dashboard * My cars * My tracks * Settings * About

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Attributes
+    @State private var selection = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection) {
+            Text("Dashboard").tabItem {
+                Text("Dashboard")
+            }.tag(0)
+            Text("My cars").tabItem {
+                Text("My cars")
+            }.tag(1)
+            Text("My tracks").tabItem {
+                Text("My tracks")
+            }.tag(2)
+            Text("Settings").tabItem {
+                Text("Settings")
+            }.tag(3)
+            Text("About").tabItem {
+                Text("About")
+            }.tag(3)
+        }
     }
 }
 
