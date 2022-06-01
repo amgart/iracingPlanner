de49cc438bcec347da2f92415ce92960d0856fba
amartinez3 <amartinez3@werfen.com>
2022-06-01 10:47:18 +0200

    Adding Codacy Code Quality badge and Codacy Pull Requests integration

---
+++ src/test/java/org/amg/iRacingPlanner/view/MainViewTest.java
@@ -0,0 +1,13 @@
+package org.amg.iRacingPlanner.view;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MainViewTest {
+
+  @Test
+  public void helloWorldTest() {
+    assertTrue(true);
+  }
+}
