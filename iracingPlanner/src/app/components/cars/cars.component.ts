310f722333dbf2b281e9f5767bca44f2a24da6e2
amartinez3 <amartinez3@werfen.com>
2022-10-05 11:24:17 +0200

    Static code analysis

--- iracingPlanner/src/app/components/cars/cars.component.ts
+++ iracingPlanner/src/app/components/cars/cars.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {CarService} from '../../services/car/car.service';
 
 @Component({
--- iracingPlanner/src/app/components/tracks/tracks.component.ts
+++ iracingPlanner/src/app/components/tracks/tracks.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {TrackService} from '../../services/track/track.service';
 
 @Component({
